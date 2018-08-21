82679b7191461e0f4c58e27b59cba1cc2fe715c9
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2018-08-21 10:37:12 +0530

    Create 2.add.c

---
+++ tutorial/2.add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    printf("Enter the value of b: ");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("sum of %d and %d is: %d",a,b,sum);
+    return 0;
+}
