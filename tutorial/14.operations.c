82dba7f1a5e0e7391dc529a247afe068ebf6e505
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2018-08-25 11:38:45 +0530

    Create 14.operations.c

---
+++ tutorial/14.operations.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int num[] = {1,2,3,4};
+    printf(num);
+    printf(ascending(num));
+    return 0;
+}
+
+int ascending(int num[]){
+    int i;
+    int size = 4;
+    printf("%d",num);
+    
+    
+    return 0;
+}
+
