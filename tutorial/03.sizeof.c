ad7e3778a4630e394327f7fc958153d63b04025b
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2018-10-10 10:04:50 +0530

    Create 03.sizeof.c

---
+++ tutorial/03.sizeof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Size of char: %d \n",sizeof(char));
+    printf("Size of int: %d \n",sizeof(int));
+    printf("Size of float: %d \n",sizeof(float));
+    printf("Size of double: %d \n",sizeof(double));
+    return 0;
+}
