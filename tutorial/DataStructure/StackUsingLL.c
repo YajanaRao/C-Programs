04addd8bc9175c2349b3fd7b8ce05f03d893d602
Yajana N Rao <yajananrao@gmail.com>
2019-12-22 16:32:52 +0530

    Create StackUsingLL.c

---
+++ tutorial/DataStructure/StackUsingLL.c
@@ -0,0 +1,47 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int length=2;
+
+struct node { 
+	int data; 
+	struct node* link; 
+} *top;
+
+void push(int data){
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->link = top;
+    top = n;
+}
+
+int pop(){
+    if(top == NULL){
+        printf("Stack is empty");
+    }else {
+        int temp = top->data;
+        top = top->link;
+        return temp;
+    }
+}
+void list(){
+    struct node *ptr;
+    ptr = top;
+    while(ptr != NULL){
+        printf("%d\n", ptr->data);
+        ptr = ptr->link;
+    }
+}
+
+int main() 
+{ 
+	push(2);
+	push(3);
+	list();
+	printf("%d\n",pop());
+	push(4);
+	push(5);
+	list();
+	return 0; 
+} 
