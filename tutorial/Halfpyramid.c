fa1cb5bae391dcab35a8fec549be621b6f80d898
Deepak Naik <34372274+deepaknaikhnvrm@users.noreply.github.com>
2020-10-27 11:53:41 +0530

    Update Halfpyramid.c

--- tutorial/Halfpyramid.c
+++ tutorial/Halfpyramid.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-   int i, j, rows;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i) {
+   int i, j, numberofRows;
+   printf("Plese enter the number of rows: ");
+   scanf("%d", &numberofRows);
+   for (i = 1; i <= numberofRows; ++i) {
       for (j = 1; j <= i; ++j) {
          printf("* ");
       }
